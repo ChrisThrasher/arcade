3734b01b372f5e62ff4d0d133d1c486f589be660
Chris Thrasher <chris.j.thrasher@gmail.com>
2021-02-25 20:46:54 -0700

    Cube Timer: Generate better scrambles

--- cube_timer/Scramble.cpp
+++ cube_timer/Scramble.cpp
@@ -1,24 +1,32 @@
 #include "Scramble.h"
 
 #include <random>
-#include <stdexcept>
 
-static constexpr std::array g_moves
-    = { "U", "U'", "U2", "F", "F'", "F2", "L", "L'", "L2", "R", "R'", "R2", "B", "B'", "B2", "D", "D'", "D2" };
+auto GenerateMove() -> std::string
+{
+    constexpr std::array moves
+        = { "U", "U'", "U2", "F", "F'", "F2", "L", "L'", "L2", "R", "R'", "R2", "B", "B'", "B2", "D", "D'", "D2" };
+
+    static std::random_device s_rd;
+    static std::mt19937 s_rng(s_rd());
+    static std::uniform_int_distribution<size_t> s_dist(0, moves.size() - 1);
+
+    return moves[s_dist(s_rng)];
+}
 
 auto GenerateScramble() -> Scramble
 {
-    static std::random_device s_rd;
-    static std::mt19937 s_rng(s_rd());
-    static std::uniform_int_distribution<size_t> s_dist(0, g_moves.size() - 1);
 
+    // This is a bit ugly
     auto scramble = Scramble {};
-    scramble[0] = g_moves[s_dist(s_rng)];
-    for (size_t i = 1; i < scramble.size(); ++i) {
-        auto move = g_moves[s_dist(s_rng)];
-        while (move[0] == scramble[i - 1][0])
-            move = g_moves[s_dist(s_rng)];
-        scramble[i] = move;
+    scramble[0] = GenerateMove();
+    scramble[1] = GenerateMove();
+    while (scramble[1][0] == scramble[0][0])
+        scramble[1] = GenerateMove();
+    for (size_t i = 2; i < scramble.size(); ++i) {
+        scramble[i] = GenerateMove();
+        while (scramble[i][0] == scramble[i - 1][0] || scramble[i][0] == scramble[i - 2][0])
+            scramble[i] = GenerateMove();
     }
 
     return scramble;
