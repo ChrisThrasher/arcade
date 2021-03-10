e90b73fdf34c141502ec142726a9882e6cca2d78
Chris Thrasher <chris.j.thrasher@gmail.com>
2021-03-09 20:49:21 -0700

    Cube Timer: Simplify scrambling algorithms

--- games/cube_timer/Scramble.cpp
+++ games/cube_timer/Scramble.cpp
@@ -1,60 +1,50 @@
 #include "Scramble.h"
 
 #include <array>
-#include <optional>
+#include <functional>
 #include <random>
 
 static std::random_device g_rd;
 static std::mt19937 g_rng(g_rd());
 
-auto Generate2x2Move(const std::optional<std::string>& prev1 = std::nullopt) -> std::string
+static auto Generate2x2Move() -> std::string
 {
     constexpr std::array moves = { "U", "U'", "U2", "F", "F'", "F2", "R", "R'", "R2" };
 
     static std::uniform_int_distribution<size_t> s_dist(0, moves.size() - 1);
+    static std::string prev;
 
     std::string move = moves[s_dist(g_rng)];
-    while (move[0] == prev1.value_or("")[0])
+    while (move[0] == prev[0])
         move = moves[s_dist(g_rng)];
+
+    prev = move;
     return move;
 }
 
-auto Generate2x2Scramble()
-{
-    constexpr auto length = 9;
-
-    auto scramble = Scramble {};
-    scramble.push_back(Generate2x2Move());
-    for (size_t i = 1; i < length; ++i)
-        scramble.push_back(Generate2x2Move(scramble[i - 1]));
-
-    return scramble;
-}
-
-auto Generate3x3Move(const std::optional<std::string>& prev1 = std::nullopt,
-                     const std::optional<std::string>& prev2 = std::nullopt) -> std::string
+static auto Generate3x3Move() -> std::string
 {
     constexpr std::array moves
         = { "U", "U'", "U2", "F", "F'", "F2", "L", "L'", "L2", "R", "R'", "R2", "B", "B'", "B2", "D", "D'", "D2" };
 
     static std::uniform_int_distribution<size_t> s_dist(0, moves.size() - 1);
+    static std::string prev1;
+    static std::string prev2;
 
     std::string move = moves[s_dist(g_rng)];
-    while (move[0] == prev1.value_or("")[0] || move[0] == prev2.value_or("")[0])
+    while (move[0] == prev1[0] || move[0] == prev2[0])
         move = moves[s_dist(g_rng)];
+
+    prev2 = prev1;
+    prev1 = move;
     return move;
 }
 
-auto Generate3x3Scramble()
+static auto GenerateScramble(const size_t length, const std::function<std::string()>& generator)
 {
-    constexpr auto length = 18;
-
     auto scramble = Scramble {};
-    scramble.push_back(Generate3x3Move());
-    scramble.push_back(Generate3x3Move(scramble[0]));
-    for (size_t i = 2; i < length; ++i)
-        scramble.push_back(Generate3x3Move(scramble[i - 1], scramble[i - 2]));
-
+    for (size_t i = 0; i < length; ++i)
+        scramble.push_back(generator());
     return scramble;
 }
 
@@ -62,9 +52,9 @@ auto GenerateScramble(const Puzzle puzzle) -> Scramble
 {
     switch (puzzle) {
     case Puzzle::Cube2:
-        return Generate2x2Scramble();
+        return GenerateScramble(9, Generate2x2Move);
     case Puzzle::Cube3:
-        return Generate3x3Scramble();
+        return GenerateScramble(18, Generate3x3Move);
     case Puzzle::Cube4:
         // TODO Implement 4x4 scrambling
         return {};
